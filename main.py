e6770df55b15d6582449e73cf5d0aecda83df229
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2024-07-14 11:46:07 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+input = input("""Enter choice(Number):
+            1. Fee Section
+            2. Student Affairs
+            3. Employees
+            4. To Do list
+            5. Expenditure
+            6. History
+            7. Finance
+            8. Exit\n\t:""")
